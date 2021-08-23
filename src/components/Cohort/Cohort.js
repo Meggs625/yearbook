8d6494b8c5c229886fe70ef58d72829153700694
Megan McBride <megan.mcbride@colorado.edu>
2021-08-23 17:21:51 -0600

    Create the Cohort component and connect with Person component

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,20 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+function Cohort(props) {
+  const peopleComponents = props.data.map(individual => {
+    return (
+      <Person key={individual.id} info={individual}/>
+    )
+  })
+  return (
+    <div>
+      <h1>Hi!</h1>
+      <section>
+        {peopleComponents}
+      </section>
+   </div>
+  )
+}
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+function Person(props) {
+  console.log(props)
+
+  return (
+    <div>
+      <hi>{props.info.name}</hi>
+      <img src={props.info.photo} alt=''></img>
+      <p>{props.info.quote}</p>
+    </div>
+  )
+}
 
 export default Person;
