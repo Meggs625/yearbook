1f35659bcc7404f437aeb2533591c0d24c35a857
Megan McBride <megan.mcbride@colorado.edu>
2021-08-31 18:31:40 -0600

    Attempt to add a way to update the cars on click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cohort from '../Cohort/Cohort';
+import Form from './Form';
 import people from '../../data/yearbook-data.js';
 import './App.css';
 
@@ -12,15 +13,35 @@ class App extends Component {
     }
   }
 
+  addStudent = (studentInfo) => {
+    this.setState({students: [studentInfo, ...people.students]})
+  }
+
+  deleteStudent = (id) => {
+    const stillStudents = this.state.students.filter(student => student.id !== id);
+    this.setState({ students: stillStudents})
+  }
+
+  // updateInfo = (id, type, update) => {
+  //   const neededStudent = this.state.students.find(student => student.id === id);
+  //   const otherStudents = this.state.students.filter(student => student.id !== id);    
+  //   neededStudent[type] = update;
+  //   this.setState({ students: [...otherStudents, neededStudent]})
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         <h1>Turing Yearbook</h1>
        </header>
-       
+        <Form addStudent={this.addStudent}/>
        <Cohort data={this.state.staff} type="staff"/>
-       <Cohort data={this.state.students} type="students" />
+       <Cohort 
+       data={this.state.students} 
+       type="students" 
+       delete={this.deleteStudent}
+       />
       </div>
     );
   }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -5,12 +5,13 @@ function Person(props) {
   const quote = props.info.quote ? props.info.quote : 'Life is like a box of chocolates';
   return (
       <div className="person-card">
-        <h1 className="person-name">{props.info.name}</h1>
-        <img className="person-pic" src={props.info.photo} alt=''></img>
+        <h1 className="person-name" >{props.info.name}</h1>
+        <img className="person-pic" src={props.info.photo} alt='' ></img>
         <div>        
-          <p className="person-quote"><i>"{quote}"</i></p>
+          <p className="person-quote" ><i>"{quote}"</i></p>
           <p>{props.info.superlative}</p>
         </div>
+        {props.type === 'students' && <button onClick={() => props.delete(props.info.id)}>Delete</button>}
       </div>
   )
 }
